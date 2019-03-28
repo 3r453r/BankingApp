f6e17f3edb5adf4577700e0d105d471cb673980c
aswierkosz <aswierkosz88@gmail.com>
2019-03-28 02:13:10 +0100

    temp

---
+++ Process.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Process
+    {
+        public Step Start { get; set; }
+        public Step End { get; set; }
+        public List<Step> Connectors { get; set; } = new List<Step>();
+
+        public ProcessValidationResult Validate()
+        {
+            var result = ProcessValidationResult.None;
+            if (Start == null)
+                result = result == ProcessValidationResult.None ? ProcessValidationResult.NoStart : result | ProcessValidationResult.NoStart;
+
+            if (End == null)
+                result = result == ProcessValidationResult.None ? ProcessValidationResult.NoEnd : result | ProcessValidationResult.NoEnd;
+
+            if(!Start.Succesors.Any() || !End.Precessors.Any() || Connectors.Any(s => !s.Precessors.Any() || !s.Succesors.Any()))
+                result = result == ProcessValidationResult.None ? ProcessValidationResult.MissingConnections : result | ProcessValidationResult.MissingConnections;
+
+            //if (result != ProcessValidationResult.None)
+            //    return result;
+
+            Start.PropagateStartConnection();
+            End.PropagateEndConnection();
+            return result == ProcessValidationResult.None ? ProcessValidationResult.Ok : result;
+        }
+       
+    }
+
+    [Flags]
+    public enum ProcessValidationResult
+    {
+        None = 0,
+        Ok = 1,
+        NoStart = 2,
+        NoEnd = 4,
+        MissingConnections = 8,
+        StepsDisconnectedFromStart = 16,
+        StepsDisconnectedFromEnd = 32
+    }
+}
---
+++ Step.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Step
+    {
+        public string Name { get; set; }
+        public bool ConnectedToStart { get; set; }
+        public bool ConnectedToEnd { get; set; }
+        public List<Step> Precessors { get; set; } = new List<Step>();
+        public List<Step> Succesors { get; set; } = new List<Step>();
+
+        public void PropagateStartConnection()
+        {
+            if (ConnectedToStart || Precessors.Any(s => s.ConnectedToStart))
+            {
+                Console.WriteLine($"Propagating start in {Name}");
+                ConnectedToStart = true;
+                foreach (var step in Succesors.Where(s => !s.ConnectedToStart))
+                {
+                    step.ConnectedToStart = true;
+                    step.PropagateStartConnection();
+                }
+            }
+        }
+
+        public void PropagateEndConnection()
+        {
+            if (ConnectedToEnd || Succesors.Any(s => s.ConnectedToEnd))
+            {
+                Console.WriteLine($"Propagating end in {Name}");
+                ConnectedToEnd = true;
+                foreach (var step in Precessors.Where(s => !s.ConnectedToEnd))
+                {
+                    step.ConnectedToEnd = true;
+                    step.PropagateEndConnection();
+                }
+            }
+        }
+    }
+}
